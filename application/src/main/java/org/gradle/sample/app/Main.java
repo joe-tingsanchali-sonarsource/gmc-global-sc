841cda5882137ce60ecb86c89718b352a1eb5abb
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2021-10-10 22:27:58 -0500

    remove github actions for now

--- application/src/main/java/org/gradle/sample/app/Main.java
+++ application/src/main/java/org/gradle/sample/app/Main.java
@@ -7,9 +7,15 @@ import static org.gradle.sample.utilities.StringUtils.split;
 import static org.gradle.sample.app.MessageUtils.getMessage;
 
 public class Main {
+    public static String getGreeting() {
+        return "Hello world.";
+    }
+
     public static void main(String[] args) {
         LinkedList tokens;
         tokens = split(getMessage());
+
         System.out.println(join(tokens));
+        System.out.println(getGreeting());
     }
 }
